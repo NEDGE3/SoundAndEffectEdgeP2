7a8349896fac748ae615c08adb159e3ab1e8cfcb
NEDGE3 <739516@student.dusd.net>
2023-03-24 09:53:55 -0700

    Continued working
    finished lesson 3.1 and started 3.2

--- SoundAndEffectEdgeP2/Assets/Scripts/PlayerController.cs
+++ SoundAndEffectEdgeP2/Assets/Scripts/PlayerController.cs
@@ -5,17 +5,28 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    public float jumpForce = 10;
+    public float gravityModifier;
+    public bool isOnGround = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerRb = GetComponent<RigidBody>();
-        playerRb.AddForce(Vector3.up * 1000);
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
